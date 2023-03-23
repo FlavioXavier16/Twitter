d854d9ea1a12e451ecda325fe095237627aa4738
Flavio Xavier <flavio.paiva@etec.sp.gov.br>
2023-03-23 12:14:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+</head>
+<body>
+    
+    <aside id="socialista">
+        <img src="image.png" alt="" width=40px>
+        <h1># Explorar</h1>
+        <h1><img src="engre.png" width=40px> Configurações </h1>
+    </aside>
+
+    <aside id="centrao">
+        B
+    </aside>
+
+    <aside id="capitalista">
+        <section id="caixinha">
+           <h1>Novo no Twitter?</h1>
+           <p class="pp">Inscreva-se para ter sua própria timeline personalizada!</p>
+           <div id="gSignInWrapper">
+    <div id="customBtn" class="customGPlusSignIn">
+      <span class="icon"></span>
+      <span class="buttonText"> Inscrever-se com o Google</span>
+    </div>
+  </div>
+  <div id="name"></div>
+  <div id="gSignInWrapper">
+    <div id="customBtn" class="customGPlusSignIn">
+      <span class="icon"></span>
+      <span class="buttonText"> Inscrever-se com Apple </span>
+    </div>
+  </div>
+  <div id="name"></div>
+  <div id="gSignInWrapper">
+    <div id="customBtn" class="customGPlusSignIn">
+      <span class="icon"></span>
+      <span class="buttonText"> Criar Conta</span>
+    </div>
+  </div>
+  <div id="name"></div>
+        </section>
+    </aside>
+    <footer>
+        Não perca o que está acontecendo
+    </footer>
+</body>
+</html>
